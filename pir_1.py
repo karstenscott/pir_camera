865975ba0af20a55c4da1a728782be995829c8e2
karstenscott <karstenwscott@gmail.com>
2015-01-03 13:31:20 -0800

    Infrared triggered PiCam

---
+++ pir_1.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#|R|a|s|p|b|e|r|r|y|P|i|-|S|p|y|.|c|o|.|u|k|
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#
+# pir_1.py
+# Detect movement using a PIR module
+#
+# Author : Matt Hawkins
+# Date   : 21/01/2013
+
+# Import required Python libraries
+import RPi.GPIO as GPIO
+import time
+import picamera
+
+# Use BCM GPIO references
+# instead of physical pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# Define GPIO to use on Pi
+GPIO_PIR = 7
+
+print "Wilbur Cam! (CTRL-C to exit)"
+
+# Set pin as input
+GPIO.setup(GPIO_PIR,GPIO.IN)      # Echo
+
+Current_State  = 0
+Previous_State = 0
+
+camera = picamera.PiCamera()
+
+try:
+
+  print "Waiting for PIR to settle ..."
+
+  # Loop until PIR output is 0
+  while GPIO.input(GPIO_PIR)==1:
+    Current_State  = 0    
+
+  print "  Ready"     
+    
+  # Loop until users quits with CTRL-C
+i = 0  # set i to 0 outside the while
+while True :    
+    # Read PIR state
+    Current_State = GPIO.input(GPIO_PIR)    
+    if Current_State==1 and Previous_State==0:
+      # PIR is triggered
+      print "  Motion detected!"
+      # Record previous state
+      Previous_State=1
+      # Camera begins to record
+      camera.resolution = (1360, 768)
+      camera.start_recording('pivid{}.h264'.format(i)) # pass i to str.format
+      print('Captured pivid{}.264'.format(i))
+      i += 1 # increase i
+      time.sleep(20)
+      camera.stop_recording()
+    elif Current_State==0 and Previous_State==1:
+      # PIR has returned to ready state
+      print "  Ready"
+      Previous_State=0
+      
+    # Wait for 10 milliseconds
+    time.sleep(0.01)
+      
+except KeyboardInterrupt:
+  print "  Quit" 
+  # Reset GPIO settings
+  GPIO.cleanup()
